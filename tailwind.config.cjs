c8f02e8a56c0865b1d697b83b335344b6e375eaf
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-08 06:02:57 +0900

    feat: draft navbar and hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Anuphan Variable'", ...defaultTheme.fontFamily.sans],
+        serif: ["'IBM Plex Serif'", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
